6dd022171608ed0612ab760f1f5e288393cecf39
nikola <nikola649@gmail.com>
2018-04-14 19:16:27 +0100

    dodaj 7

---
+++ zad7/src/zad7/Zad7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zad7;
+
+public class Zad7 {
+
+   
+    public static void main(String[] args) {
+        int[] array = {1,2, 3,0 ,7,8,9,10};
+        for(int i = 1; i < array.length; i=i+2)
+        {
+           System.out.print(array[i]+" ");
+        }}
+    
+    
+}
