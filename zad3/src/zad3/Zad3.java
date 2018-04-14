bb59d72983e873fd54b90cc8d725c8c94aef8a3e
nikola <nikola649@gmail.com>
2018-04-14 18:24:20 +0100

    add zad3

---
+++ zad3/src/zad3/Zad3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zad3;
+
+/**
+ *
+ * @author Nikola
+ */
+public class Zad3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int a;
+int b;
+for (a=1; a<=12; ++a)
+{
+for (b=1; b<=12; ++b)
+{
+System.out.printf("%4d",a*b);  
+}
+System.out.println();
+}    
+    }
+    
+}
